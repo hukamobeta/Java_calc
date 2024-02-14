2ec5842abd3ecc3e6444881c6cd96ce5ce8016b1
hukamobeta <m.kukin@avsw.ru>
2024-02-14 12:21:12 +0300

    Add normal excel export

--- ExcelExporter.java
+++ ExcelExporter.java
@@ -1,3 +1,6 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileOutputStream;
 import java.sql.*;
 
 public class ExcelExporter {
@@ -6,15 +9,41 @@ public class ExcelExporter {
         try (Connection conn = DBhandler.getConnection();
              Statement stmt = conn.createStatement();
              ResultSet rsTables = stmt.executeQuery(sqlTables)) {
-
             while (rsTables.next()) {
                 String table = rsTables.getString(1);
-                String query = "SELECT * FROM " + table + " INTO OUTFILE '/Users/maksimkukin/Java_finashka/Java_calc/result/" + table + ".csv'";
+                String query = "SELECT * FROM " + table;
                 PreparedStatement statement = conn.prepareStatement(query);
-                statement.executeQuery();
+                ResultSet resultSet = statement.executeQuery();
+
+                Workbook workbook = new XSSFWorkbook();
+                Sheet sheet = workbook.createSheet(table);
+
+                Row headerRow = sheet.createRow(0);
+                ResultSetMetaData rsmd = resultSet.getMetaData();
+                int columnCount = rsmd.getColumnCount();
+
+                for (int i = 1; i <= columnCount; i++) {
+                    Cell cell = headerRow.createCell(i - 1);
+                    cell.setCellValue(rsmd.getColumnName(i));
+                }
+
+                int rowIndex = 1;
+                while (resultSet.next()) {
+                    Row row = sheet.createRow(rowIndex++);
+
+                    for (int i = 1; i <= columnCount; i++) {
+                        Cell cell = row.createCell(i - 1);
+                        cell.setCellValue(resultSet.getString(i));
+                    }
+                }
+
+                try (FileOutputStream out = new FileOutputStream("/Users/maksimkukin/Java_finashka/Java_calc/result/" + table + ".xlsx")) {
+                    workbook.write(out);
+                }
+
+                workbook.close();
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
